a3297d7dfd488e37cf3a94de4b24307e985fe8f8
letuananh <le.tuan.anh@aainc.co.jp>
2017-03-23 19:25:02 +0900

    add softmax

--- src/activations/softmax.py
+++ src/activations/softmax.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+from torch.nn.modules.module import Module
+from torch.autograd import Function, Variable
+import torch
 
 def softmax_loss(x, y):
     """
@@ -23,3 +25,21 @@ def softmax_loss(x, y):
     dx[np.arange(N), y] -= 1
     dx /= N
     return loss, dx
+
+
+class SoftMaxFunction(Function):
+    """docstring for SoftMaxFunction"""
+
+    def forward(self, input, ):
+        input = input.numpy()
+        probs = np.exp(input - np.max(input, axis=1, keepdims=True))
+        probs /= np.sum(probs, axis=1, keepdims=True)
+        return torch.FloatTensor(probs)
+
+    def backward(self, grad_output):
+        probs = self.saved_tensors
+        probs = probs.numpy()
+        grad_output = grad_output.numpy()
+
+        d_input = probs.copy()
+        d_input[np.arange(N), ]
