56783388adb13d4dfde1b7ab775767ea9d739877
Le Anh <tuananh.kirimaru@gmail.com>
2017-04-02 01:56:43 +0900

    add Solver

--- src/solver.py
+++ src/solver.py
@@ -1,4 +1,7 @@
 from torch import optim
+from torch.utils.data.dataloader import DataLoaderIter, DataLoader
+from torch.autograd import Variable
+import torch
 
 
 class Solver(object):
@@ -6,15 +9,16 @@ class Solver(object):
 
     def __init__(self, model, data, **kwargs):
         super(Solver, self).__init__()
+        print("init")
         self.model = model
-        self.data = data
 
         self.optimizer = kwargs.pop('optimizer', 'Adam')
         self.optim_config = kwargs.pop('optim_config', {})
         self.batch_size = kwargs.pop('batch_size', 100)
         self.num_epochs = kwargs.pop('num_epochs', 10)
         self.print_every = kwargs.pop('print_every', 10)
-        self.verbose = kwargs.pop('verbose', '')
+        self.verbose = kwargs.pop('verbose', True)
+        self.loss = kwargs.pop('loss', '')
 
         if(len(kwargs) > 0):
             extra = ', '.join('"%s"' % k for k in kwargs.keys())
@@ -24,6 +28,13 @@ class Solver(object):
             raise ValueError('Invalid optimizer "%s"' % self.optimizer)
         self.optimizer = getattr(optim, self.optimizer)(
             model.parameters(), **self.optim_config)
+
+        self.loss_fn = self.loss()
+
+        # Convert data to iter
+        data_loader = DataLoader(data, batch_size=30, shuffle=True)
+        self.data = DataLoaderIter(data_loader)
+        self._reset()
 
     def _reset(self):
         """
@@ -38,16 +49,13 @@ class Solver(object):
         self.train_acc_history = []
         self.val_acc_history = []
 
-        self.optim_config = {}
-
-        for p in self.model.params:
-            d = {k: v for k, v in self.optim_config.iteritems()}
-            self.optim_config[p] = d
-
     def _step(self):
 
-        input, output = self.data.get()
-        loss = self.model.loss(input, output)
+        input, target = self.data.next()
+        input, target = Variable(input), Variable(
+            target.type(torch.LongTensor).squeeze())
+        output = self.model(input)
+        loss = self.loss_fn(output, target)
         self.loss_history.append(loss)
         loss.backward()
         self.optimizer.zero_grad()
@@ -55,16 +63,17 @@ class Solver(object):
 
     def train(self):
 
-        num_train = self.data.size()
+        num_train = len(self.data)
         iterations_per_epoch = max(num_train / self.batch_size, 1)
         num_iterations = self.num_epochs * iterations_per_epoch
 
         for t in xrange(num_iterations):
             self._step()
 
-            if self.verbose and t % self.print_every:
+            if self.verbose and t % self.print_every == 0:
                 print '(Iteration %d / %d) loss: %f' % (
-                    t + 1, num_iterations, self.loss_history[-1])
+                    t + 1, num_iterations,
+                    self.loss_history[-1].data.numpy()[0])
 
             epoch_end = (t + 1) % iterations_per_epoch == 0
 
