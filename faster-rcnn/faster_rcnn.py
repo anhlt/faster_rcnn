9916df445c01a7fe1c30054e3a64850926170d29
anhlt <tuananh.kirimaru@gmail.com>
2017-06-11 02:25:14 +0900

    define Conv2D define FC

--- faster-rcnn/faster_rcnn.py
+++ faster-rcnn/faster_rcnn.py
@@ -1,4 +1,14 @@
 import cv2
 import numpy as numpy
-import torch 
-import torch.nn as nn 
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from utils.timer import Timer
+from utils.blob import im_list_to_blob
+
+from lib.nms_wrapper import nms
+from rpn_msr.proposal_layer import proposal_layer as proposal_layer_py
+from rpn_msr.anchor_target_layer import anchor_target_layer as anchor_target_layer_py
+from rpn_msr.proposal_target_layer import proposal_target_layer as proposal_target_layer_py
+from lib.bbox_transform import bbox_transform_inv, clip_boxes
---
+++ faster-rcnn/network.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class Conv2d(nn.Module):
+    """docstring for Conv2d"""
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 relu=True,
+                 same_padding=False,
+                 bn=False):
+
+        super(Conv2d, self).__init__()
+        padding = int((kernel_size - 1) / 2) if same_padding else 0
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                              kernel_size, stride, padding=padding)
+        self.bn = nn.BatchNorm2d(
+            out_channels, eps=0.001,
+            momentum=0,
+            affine=True
+        ) if bn else None
+
+        self.relu = nn.ReLU(inplace=True) if relu else None
+
+        def forward(self, x):
+            x = self.conv(x)
+            if self.bn is not None:
+                x = self.bn(x)
+            if self.relu is not None:
+                x = self.relu(x)
+
+
+class FC(nn.Module):
+    """docstring for FC"""
+
+    def __init__(self,
+                 in_features,
+                 out_features,
+                 relu=True
+                 ):
+        super(FC, self).__init__()
+        self.fc = nn.Linear(in_features, out_features)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.fc(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+
